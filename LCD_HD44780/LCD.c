56f97230b1ec21298a4471059791b4815c2ad5c9
Tunachi <lukasz.tomczyk50@gmail.com>
2017-06-30 18:21:54 +0200

    Template for LCD library

--- LCD_HD44780/LCD.c
+++ LCD_HD44780/LCD.c
@@ -5,25 +5,23 @@
  *  Author: Tunachi
  */ 
 
+ /*****************************************************************************
+                  Include files
+*****************************************************************************/
  #include <avr/io.h>
  #include <util/delay.h>
  #include <avr/pgmspace.h>
  #include <avr/eeprom.h>
  #include <stdlib.h>
  #include "LCD.h"
+ /*****************************************************************************
+                  Local types, enums definitions
+*****************************************************************************/
 
- static void LCD_InitPort(void);
- static inline void LCD_DataDirOut(void);
- static inline void LCD_DataDirIn(void);
- static inline void LCD_SendHalf(uint8_t data);
- static inline uint8_t LCD_ReadHalf(void);
- static uint8_t LCD_ReadByte(void);
- static uint8_t LCD_Check_BF(void);
- static void LCD_WraiteByte(uint8_t data);
- void LCD_WriteCmd(uint8_t cmd);
- void LCD_WriteData(uint8_t data);
 
- /*------------------------------ Control Pins macro definitions ----------------------*/
+/*****************************************************************************
+                  Local symbolic constants
+*****************************************************************************/
  //RS = 1 - command register
  #define SET_RS LCD_PORT |= (1<<LCD_RS)
  //RS = 0 - data register
@@ -36,7 +34,53 @@
 
  #define SET_E LCD_PORT |= (1<<LCD_E)
  #define CLR_E LCD_PORT &= ~(1<<LCD_E)
- /*------------------------------------------------------------------------------------*/
+ /*****************************************************************************
+                  Exported object definitions
+*****************************************************************************/
+
+
+/*****************************************************************************
+                  Local object definitions
+*****************************************************************************/
+
+
+/*****************************************************************************
+                  Local defined macros
+*****************************************************************************/
+
+
+/*****************************************************************************
+                  Local function prototypes
+*****************************************************************************/
+ static void LCD_InitPort(void);
+ static inline void LCD_DataDirOut(void);
+ static inline void LCD_DataDirIn(void);
+ static inline void LCD_SendHalf(uint8_t data);
+ static inline uint8_t LCD_ReadHalf(void);
+ static uint8_t LCD_ReadByte(void);
+ static uint8_t LCD_Check_BF(void);
+ static void LCD_WraiteByte(uint8_t data);
+ void LCD_WriteCmd(uint8_t cmd);
+ void LCD_WriteData(uint8_t data);
+ /*****************************************************************************
+                  Function definitions
+*****************************************************************************/
+
+/*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_InitPort
+*
+* FUNCTION ARGUMENTS:
+*    None
+*
+* RETURN VALUE:
+*    None
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_InitPort initialize LCD pins and set low state on LCD control pins
+*
+*---------------------------------------------------------------------------*/
  static void LCD_InitPort(void)
  {
 	//LCD Pins as outputs
@@ -47,16 +91,61 @@
 	LCD_PORT &= ~((1<<LCD_RS) | (1<<LCD_RW) | (1<<LCD_E));
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_DataDirOut
+*
+* FUNCTION ARGUMENTS:
+*    None
+*
+* RETURN VALUE:
+*    None
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_DataDirOut set data pins(D4..D7) direction as output
+*
+*---------------------------------------------------------------------------*/
  static inline void LCD_DataDirOut(void)
  {
 	LCD_DDR |= (1<<LCD_D4) | (1<<LCD_D5) | (1<<LCD_D6) | (1<<LCD_D7);
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_DataDirIn
+*
+* FUNCTION ARGUMENTS:
+*    None
+*
+* RETURN VALUE:
+*    None
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_DataDirIn set data pins(D4..D7) direction as input
+*
+*---------------------------------------------------------------------------*/
  static inline void LCD_DataDirIn(void)
  {
 	LCD_DDR &= ~((1<<LCD_D4) | (1<<LCD_D5) | (1<<LCD_D6) | (1<<LCD_D7));
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_SendHalf
+*
+* FUNCTION ARGUMENTS:
+*    data - value that will be send to LCD
+*
+* RETURN VALUE:
+*    None
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_SendHalf set data pins to send four bits to the LCD
+*
+*---------------------------------------------------------------------------*/
  static inline void LCD_SendHalf(uint8_t data)
  {
 	if(data & (1<<0))
@@ -77,6 +166,21 @@
 		LCD_PORT &= ~(1<<LCD_D7);
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_ReadHalf
+*
+* FUNCTION ARGUMENTS:
+*    None
+*
+* RETURN VALUE:
+*    result - four bits read from LCD
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_ReadHalf check data pins and four bits from LCD
+*
+*---------------------------------------------------------------------------*/
  static inline uint8_t LCD_ReadHalf(void)
  {
 	uint8_t result = 0;
@@ -89,6 +193,21 @@
 	return result;
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_ReadByte
+*
+* FUNCTION ARGUMENTS:
+*    None
+*
+* RETURN VALUE:
+*    result - byte read form LCD
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_ReadByte read byte from LCD
+*
+*---------------------------------------------------------------------------*/
  static uint8_t LCD_ReadByte(void)
  {
 	uint8_t result = 0;
@@ -107,12 +226,42 @@
 	return result;
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_Check_BF
+*
+* FUNCTION ARGUMENTS:
+*    None
+*
+* RETURN VALUE:
+*    Return "0" if LCD can accept next command 
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_Check_BF check if LCD is busy
+*
+*---------------------------------------------------------------------------*/
  static uint8_t LCD_Check_BF(void)
  {
 	CLR_RS;
 	return LCD_ReadByte();
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_WraiteByte
+*
+* FUNCTION ARGUMENTS:
+*    data - value that will be written do the LCD
+*
+* RETURN VALUE:
+*    None
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_WraiteByte write data byte to the LCD
+*
+*---------------------------------------------------------------------------*/
  static void LCD_WraiteByte(uint8_t data)
  {
 	LCD_DataDirOut();
@@ -131,18 +280,63 @@
 		;
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_WriteCmd
+*
+* FUNCTION ARGUMENTS:
+*    cmd - command that will be wtitten to the LCD
+*
+* RETURN VALUE:
+*    None
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_WriteCmd write command to the LCD
+*
+*---------------------------------------------------------------------------*/
  void LCD_WriteCmd(uint8_t cmd)
  {
 	CLR_RS;
 	LCD_WraiteByte(cmd);
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_WriteData
+*
+* FUNCTION ARGUMENTS:
+*    data - data that will be wtitten to the LCD
+*
+* RETURN VALUE:
+*    None
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_WriteData write data to the LCD
+*
+*---------------------------------------------------------------------------*/
  void LCD_WriteData(uint8_t data)
  {
 	SET_RS;
 	LCD_WraiteByte(data);
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_Init
+*
+* FUNCTION ARGUMENTS:
+*    None
+*
+* RETURN VALUE:
+*    None
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_Init initialize LCD
+*
+*---------------------------------------------------------------------------*/
  void LCD_Init(void)
  {
 	LCD_InitPort();
@@ -159,36 +353,141 @@
 	
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_Clear
+*
+* FUNCTION ARGUMENTS:
+*    None
+*
+* RETURN VALUE:
+*    None
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_Clear clear LCD display
+*
+*---------------------------------------------------------------------------*/
  void LCD_Clear(void)
  {
 	LCD_WriteCmd(CLEAR_DISPLAY);
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_Home
+*
+* FUNCTION ARGUMENTS:
+*    None
+*
+* RETURN VALUE:
+*    None
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_Home set cursor in home position(0,0)
+*
+*---------------------------------------------------------------------------*/
  void LCD_Home(void)
  {
 	LCD_WriteCmd(CLEAR_DISPLAY | RETURN_HOME);
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_CursorOn
+*
+* FUNCTION ARGUMENTS:
+*    None
+*
+* RETURN VALUE:
+*    None
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_CursorOn turn On cursor (cursor is visible)
+*
+*---------------------------------------------------------------------------*/
  void LCD_CursorOn(void)
  {
 	LCD_WriteCmd(DISPLAY_ON_OFF_CONTROL | DISPLAY_ON | CURSOR_ON);
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_CursorOff
+*
+* FUNCTION ARGUMENTS:
+*    None
+*
+* RETURN VALUE:
+*    None
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_CursorOff turn Off cursor (cursor is not visible)
+*
+*---------------------------------------------------------------------------*/
  void LCD_CursorOff(void)
  {
 	LCD_WriteCmd(DISPLAY_ON_OFF_CONTROL | DISPLAY_ON | CURSOR_OFF);
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_CursorBlinkOn
+*
+* FUNCTION ARGUMENTS:
+*    None
+*
+* RETURN VALUE:
+*    None
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_CursorBlinkOn turn On cursor blinking
+*
+*---------------------------------------------------------------------------*/
  void LCD_CursorBlinkOn(void)
  {
 	LCD_WriteCmd(DISPLAY_ON_OFF_CONTROL | DISPLAY_ON | BLINK_CURSOR_ON);
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCDCursorBlinkOff
+*
+* FUNCTION ARGUMENTS:
+*    None
+*
+* RETURN VALUE:
+*    None
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCDCursorBlinkOff turn Off cursor blinking
+*
+*---------------------------------------------------------------------------*/
  void LCDCursorBlinkOff(void)
  {
 	LCD_WriteCmd(DISPLAY_ON_OFF_CONTROL | DISPLAY_ON | BLINK_CURSOR_OFF);
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_Str
+*
+* FUNCTION ARGUMENTS:
+*    str - pointer to string that will be displayed on LCD
+*
+* RETURN VALUE:
+*    None
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_Str write string on the LCD
+*
+*---------------------------------------------------------------------------*/
  void LCD_Str(char *str)
  {
 	register char symbol;
@@ -197,6 +496,21 @@
 		LCD_WriteData(((symbol >= 0x80) && (symbol <= 0x87)) ? (symbol & 0x07) : symbol);
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_Str_P
+*
+* FUNCTION ARGUMENTS:
+*    str - pointer to string from flash memory that will be displayed on LCD
+*
+* RETURN VALUE:
+*    None
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_Str_P write string from flash memory on LCD
+*
+*---------------------------------------------------------------------------*/
  void LCD_Str_P(const char *str)
  {
 	register char symbol;
@@ -205,6 +519,21 @@
 		LCD_WriteData(((symbol >= 0x80) && (symbol <= 0x87)) ? (symbol & 0x07) : symbol);
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_Str_E
+*
+* FUNCTION ARGUMENTS:
+*    str - pointer to string from EEPROM memory that will be displayed on LCD
+*
+* RETURN VALUE:
+*    None
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_Str_E write string from EEPROM memory on LCD
+*
+*---------------------------------------------------------------------------*/
  void LCD_Str_E(const char *str)
  {
 	register char symbol;
@@ -219,15 +548,46 @@
 	}
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_Int
+*
+* FUNCTION ARGUMENTS:
+*    value - number that will be displayed on LCD
+*
+* RETURN VALUE:
+*    None
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_Int convert decimal value to string and display it on LCD
+*
+*---------------------------------------------------------------------------*/
  void LCD_Int(int value)
  {
 	char bufor[17];
 	LCD_Str(itoa(value, bufor, 10));
  }
 
+ /*----------------------------------------------------------------------------
+*
+* FUNCTION NAME: LCD_Hex
+*
+* FUNCTION ARGUMENTS:
+*    value - number that will be displayed on LCD
+*
+* RETURN VALUE:
+*    None
+*
+* FUNCTION DESCRIPTION:
+*
+*    LCD_Hex convert hexadecimal value to string and display it on LCD
+*
+*---------------------------------------------------------------------------*/
  void LCD_Hex(int value)
  {
 	char bufor[17];
 	LCD_Str("0x");
 	LCD_Str(itoa(value, bufor, 16));
  }
+ 
